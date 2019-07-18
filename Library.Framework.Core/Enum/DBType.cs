f29414a0a16f7366b633f609e64f944ac5acfd0a
ZhangWenChuan <tracymcgrady_zwc@126.com>
2019-07-18 16:34:53 +0800

    数据库配置，mq配置

---
+++ Library.Framework.Core/Enum/DBType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Framework.Core.Enum
+{
+    public enum DBType
+    {
+        Oracle = 0,
+        SqlServer = 1,
+        MySql = 2,
+        PgSql = 3
+    }
+}
