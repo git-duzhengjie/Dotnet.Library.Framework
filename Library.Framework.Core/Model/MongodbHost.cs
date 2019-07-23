0fadf5ae52c63ff76a5b89fe2083d3b0c8861025
ZhangWenChuan <tracymcgrady_zwc@126.com>
2019-07-23 10:21:09 +0800

    mongoClient

---
+++ Library.Framework.Core/Model/MongodbHost.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Framework.Core.Model
+{
+    public class MongodbHost
+    {
+        public string Connection { get; set; }
+        public string DataBase { get; set; }
+        public string Table { get; set; }
+
+        public string DBHost { get; set; }
+        public string DBPort { get; set; }
+        public string DBUser { get; set; }
+        public string DBPwd { get; set; }
+        public string DBName { get; set; }
+        public string DBTimeOut { get; set; }
+
+    }
+}
---
+++ Library.Framework.Core/Utility/MongodbClient.cs
@@ -0,0 +1,45 @@
+﻿using Library.Framework.Core.Model;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Framework.Core.Utility
+{
+    public static class MongodbClient<T> where T : class
+    {
+        #region MongodbInfoClient 获取mongodb
+        /// <summary>
+        /// 获取mongodb实例
+        /// </summary>  
+        public static IMongoCollection<T> MongodbInfoClient(MongodbHost host)
+        {
+            try
+            {
+                var hosts = host.DBHost.Split(new char[] { '|' });
+                var servers = new List<MongoServerAddress>();
+
+                foreach (var h in hosts)
+                {
+                    servers.Add(new MongoServerAddress(h, int.Parse(host.DBPort)));
+                }
+                var mc = new MongoClient(new MongoClientSettings
+                {
+                    Servers = servers,
+                    Credentials = new List<MongoCredential> { MongoCredential.CreateCredential("admin", host.DBUser, host.DBPwd) },
+                    SocketTimeout = TimeSpan.FromSeconds(double.Parse(host.DBTimeOut))
+                });
+
+                var Database = mc.GetDatabase(host.DBName);
+                return Database.GetCollection<T>(host.Table);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+        #endregion
+    }
+
+}
